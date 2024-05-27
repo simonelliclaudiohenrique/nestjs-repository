c0860857c0047ca3f1ce8da0c4d3b44fa24b8715
claudio-henrique <simonelliclaudiohenrique@gmail.com>
2024-05-26 21:15:18 -0400

    fix: env

--- src/modules/users/__test__/user.repository.spec.ts
+++ src/modules/users/__test__/user.repository.spec.ts
@@ -3,6 +3,7 @@ import { UserRepository } from '../repositories/user.repository';
 import { User } from '../entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+const dbType = process.env.DB_TYPE as 'mysql' | 'mariadb' | 'postgres';
 describe('UserRepository', () => {
   let userRepository: UserRepository;
 
@@ -10,14 +11,14 @@ describe('UserRepository', () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
         TypeOrmModule.forRoot({
-          type: 'postgres',
-          host: 'localhost',
-          port: 5432,
-          username: 'postgres',
-          password: '123456',
-          database: 'teste',
+          type: dbType,
+          host: process.env.DB_HOST,
+          port: parseInt(process.env.DB_PORT, 10),
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
           entities: [User],
-          synchronize: true,
+          synchronize: process.env.DB_SYNCHRONIZE === 'true',
         }),
         TypeOrmModule.forFeature([User]),
       ],
--- src/shared/database/ormconfig.ts
+++ src/shared/database/ormconfig.ts
@@ -1,15 +1,17 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from '../../modules/users/entities/user.entity';
 
+const dbType = process.env.DB_TYPE as 'mysql' | 'mariadb' | 'postgres';
+
 const ormconfig: TypeOrmModuleOptions = {
-  type: 'postgres', // Define o tipo de banco de dados como PostgreSQL
-  host: 'localhost', // Endereço do servidor PostgreSQL
-  port: 5432, // Porta padrão do PostgreSQL
-  username: 'postgres', // Nome de usuário do PostgreSQL
-  password: '123456', // Senha do PostgreSQL
-  database: 'teste', // Nome do banco de dados PostgreSQL
-  entities: [User], // Entidades que serão gerenciadas pelo TypeORM
-  synchronize: true, // Sincroniza automaticamente as entidades com o banco de dados (APENAS PARA AMBIENTES DE DESENVOLVIMENTO, NÃO RECOMENDADO PARA PRODUÇÃO)
+  type: dbType,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [User],
+  synchronize: process.env.DB_SYNCHRONIZE === 'true',
 };
 
 export default ormconfig;
